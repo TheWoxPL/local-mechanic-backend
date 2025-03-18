15889ff61bee0501aa405037e7a1e6058707fe1b
TheWoxPL <wojtek.bubula@onet.pl>
2025-03-19 00:03:22 +0100

    feat: change add company endpoint from testing to valid one

--- src/core/companies/controllers/companies.controller.ts
+++ src/core/companies/controllers/companies.controller.ts
@@ -3,50 +3,22 @@ import { Post } from '@nestjs/common';
 import { Request } from 'express';
 import { CompaniesService } from '../services/companies.service';
 import { CreateCompanyDTO } from '../dtos/create-company.dto';
-import { Permissions } from '../../../libs';
+import { AppPermissions, Permissions } from '../../../libs';
 import { UpsertDefaultsService } from 'src/upsert-defaults/upsert-defaults.service';
 import { CompanyDTO } from '../dtos/company.dto';
+// import { CompanyDTO } from '../dtos/company.dto';
 
 
 @Controller('companies')
 export class CompaniesController {
   constructor(
     private readonly companieService: CompaniesService,
-    private upsertDefaultsService: UpsertDefaultsService,
   ) { }
 
-  @Permissions([])
-  @Get('add-company')
-  async addCompany(@Req() req: Request): Promise<string> {
-    const systemUser = await this.upsertDefaultsService.getSystemAccount();
-    let createCompanyDto = new CreateCompanyDTO()
-    createCompanyDto.companyName = "Mock Company";
-    createCompanyDto.description = "Mock Description";
-    createCompanyDto.foundDate = new Date();
-    createCompanyDto.owners = "Mock Owner";
-    createCompanyDto.verifiedOwners = [];
-    createCompanyDto.createdBy = systemUser;
-    createCompanyDto.updatedBy = systemUser;
-
-
-    const result = await this.companieService.create(createCompanyDto);
-    console.log(result)
-    return "sad";
+  @Permissions(AppPermissions.APP.DISPLAY)
+  @Post('add-company')
+  async addCompany(@Req() req: Request): Promise<CompanyDTO> {
+    const result = await this.companieService.create(req.body, req.session.user!.email);
+    return result;
   }
-
-  // @Post('add-customer-role')
-  // async addCustomerRole(@Req() req: Request): Promise<string> {
-  //   return await this.accountsService.addRole(
-  //     RoleType.CUSTOMER,
-  //     req.session.user!.email
-  //   );
-  // }
-
-  // @Post('add-entrepreneur-role')
-  // async addEntrepreneurRole(@Req() req: Request): Promise<string> {
-  //   return await this.accountsService.addRole(
-  //     RoleType.ENTREPRENEUR,
-  //     req.session.user!.email
-  //   );
-  // }
 }
