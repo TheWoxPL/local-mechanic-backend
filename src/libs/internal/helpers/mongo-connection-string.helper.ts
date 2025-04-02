c5b80864c865cad0815d2706d56fe5bcdb3d94f4
TheWoxPL <wojtek.bubula@onet.pl>
2025-04-02 20:35:40 +0200

    feat: connect with online mongodb atlas

--- src/libs/internal/helpers/mongo-connection-string.helper.ts
+++ src/libs/internal/helpers/mongo-connection-string.helper.ts
@@ -1,13 +1,26 @@
 import { ConfigService } from '@nestjs/config';
 
 export function getMongoConnectionString(configService: ConfigService): string {
+  const hostDev = configService.get<string>('MONGODB_HOST_DEV');
+  const portDev = configService.get<string>('MONGODB_PORT_DEV');
+  const databaseDev = configService.get<string>('MONGODB_DATABASE_DEV');
+  const userDev = configService.get<string>('MONGODB_USER_DEV');
+  const passwordDev = configService.get<string>('MONGODB_PASSWORD_DEV');
+
   const host = configService.get<string>('MONGODB_HOST');
-  const port = configService.get<string>('MONGODB_PORT');
   const database = configService.get<string>('MONGODB_DATABASE');
   const user = configService.get<string>('MONGODB_USER');
   const password = configService.get<string>('MONGODB_PASSWORD');
 
+  const isSrv = configService.get<boolean>('MONGODB_SRV');
+
+  if (isSrv) {
+    return user && password
+      ? `mongodb+srv://${user}:${password}@${host}/${database}?retryWrites=true&w=majority&appName=local-mechanic-service`
+      : `mongodb+srv://${host}/${database}?retryWrites=true&w=majority&appName=local-mechanic-service`;
+  }
+
   return user && password
-    ? `mongodb://${user}:${password}@${host}:${port}/${database}?authSource=admin`
-    : `mongodb://${host}:${port}/${database}`;
+    ? `mongodb://${userDev}:${passwordDev}@${hostDev}:${portDev}/${databaseDev}?authSource=admin`
+    : `mongodb://${hostDev}:${portDev}/${databaseDev}`;
 }
